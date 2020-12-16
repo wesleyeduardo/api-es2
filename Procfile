5c42c10d4359384b621c09e1c513374e67c60af9
wesleyeduardocr7 <wesley.eduardo@acad.ifma.edu.br>
2020-12-16 19:11:51 -0300

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java  -jar -Dserver.port=$PORT -Dspring.profiles.active=prod target/estoque-0.0.1-SNAPSHOT.jar
---
+++ src/test/java/com/example/estoque/EstoqueApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.estoque;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EstoqueApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        Assert.assertTrue(true);
+    }
+
+}
